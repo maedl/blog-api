9cd680c3b5c3bb6afa420d0fd81f99a0f00501dd
Max Edlund <maxedlund88@gmail.com>
2023-09-25 00:40:22 +0200

    build

---
+++ dist/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logTime = void 0;
+const logTime = () => {
+    return new Date().toLocaleTimeString();
+};
+exports.logTime = logTime;
--- dist/index.js
+++ dist/index.js
@@ -5,12 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const functions_1 = require("./functions");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript server');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('hello');
 });
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server] ${(0, functions_1.logTime)()}: Server is running at http://localhost:${port}`);
 });
