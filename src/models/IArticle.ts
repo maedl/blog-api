5b7b3458cd18b6454a58693455fd1e472475ca39
Max Edlund <maxedlund88@gmail.com>
2023-09-25 01:01:30 +0200

    add article interface and mock db array

---
+++ src/models/IArticle.ts
@@ -0,0 +1,43 @@
+export interface IArticle {
+  id: string;
+  title: string;
+  description: string;
+  content: string[];
+  likes: number;
+}
+
+export const mockArticles: IArticle[] = [
+  {
+    id: '1',
+    title: 'Headline with great info 1',
+    description: 'I describe this article',
+    content: [
+      'Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet.',
+    ],
+    likes: 0,
+  },
+  {
+    id: '2',
+    title: 'Headline with great info 2',
+    description: 'I describe this article',
+    content: [
+      'Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet.',
+    ],
+    likes: 0,
+  },
+  {
+    id: '3',
+    title: 'Headline with great info 3',
+    description: 'I describe this article',
+    content: [
+      'Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet.',
+      'Lorem ipsum dolor sit amet.',
+    ],
+    likes: 0,
+  },
+];
